5231ea5e9ab4b1982c81c21af9891434cfba4090
Overseer1 <lizaso.157383@stamaria.sti.edu.ph>
2024-06-09 20:37:24 +0800

    firstCommit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import "../app.css";
+    // import Footer from "../routes/components/Footer.svelte";
+</script>
+<div>
+    <!-- <div class="sticky top-0 flex">
+		<NavBar/>
+	</div> -->
+	<main class="sticky top-0">
+		<slot />
+	</main>
+	<!-- <div class="text-xl text-white text-center fixed inset-x-0 bottom-0 p-1 bgFooter">
+		<Footer />
+	</div> -->
+</div>
+<style lang="postcss">
+	.bgFooter {
+      color: #dddddd;
+    }
+</style>
